e6affd67c960a69b11291a645bb40c97c2c8bcc7
John Torres <John.Torres@endava.com>
2019-02-01 11:30:54 -0500

    improve presentation

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner is = new Scanner(System.in);
-        int n = is.nextInt();
         GozintaChain loadProblem = new GozintaChain();
-        List<List<Integer>> chain = loadProblem.solve(n);
-        System.out.println(chain.size());
-        System.out.println(chain);
+
+        System.out.println("Enter the number:\t");
+        int n = is.nextInt();
+        List<List<Integer>> chains = loadProblem.solve(n);
+        System.out.println("The number g(n) is:" + chains.size());
+        System.out.println("The chains are:");
+        for (List<Integer> chain: chains)
+            System.out.println(chain);
     }
 }
